ffd6954b60f3b075048ce5b4175b2852201a0fda
Astrid TCHOUTENG <astrid.tchouteng@gmail.com>
2018-08-28 10:09:25 +0200

    Commit initial du back

---
+++ src/main/java/be/pizzaland/PizzaLandBackApplication.java
@@ -0,0 +1,12 @@
+package be.pizzaland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaLandBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaLandBackApplication.class, args);
+	}
+}
