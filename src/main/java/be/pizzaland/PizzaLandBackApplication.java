b17ddf42cc6036cb51825d63961e368e879057e0
Herve Ekem <ekemherve@gmail.com>
2018-08-29 15:55:18 +0200

    test

--- src/main/java/be/pizzaland/PizzaLandBackApplication.java
+++ src/main/java/be/pizzaland/PizzaLandBackApplication.java
@@ -26,6 +26,7 @@ public class PizzaLandBackApplication {
 	
 	
 	public static void main(String[] args) {
+		System.out.println("=================  hello =================");
 		SpringApplication.run(PizzaLandBackApplication.class, args);
 	
 	}
